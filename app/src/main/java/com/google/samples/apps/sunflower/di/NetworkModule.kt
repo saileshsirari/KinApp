c88642a98e747f66d8d1eba9fe9c9e90b283dcbc
ssirari <o0852@alhilabank.ae>
2022-04-09 22:48:37 +0400

    cleanup

--- app/src/main/java/com/google/samples/apps/sunflower/di/NetworkModule.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/di/NetworkModule.kt
@@ -17,7 +17,6 @@
 package com.google.samples.apps.sunflower.di
 
 import com.google.samples.apps.sunflower.api.CaseStudiesService
-import com.google.samples.apps.sunflower.api.UnsplashService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,12 +26,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 class NetworkModule {
-
-    @Singleton
-    @Provides
-    fun provideUnsplashService(): UnsplashService {
-        return UnsplashService.create()
-    }
 
     @Singleton
     @Provides
